a3448e4e513abbd429932f7362897d5fe7ea516e
Pavana K V <pavanakv557@gmail.com>
2025-05-15 21:59:37 +0530

    First commit

---
+++ core_java/src/com/atria/day1/FirstProgram.java
@@ -0,0 +1,13 @@
+package com.atria.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("my first program");
+		System.out.println("C2tc program");
+		System.out.print("java programming ");
+		System.out.println(" from tns ");
+
+	}
+
+}
